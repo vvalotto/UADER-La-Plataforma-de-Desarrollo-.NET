9eea4a25702247c2c7084ab1429e133e3711ddb5
Victor Valotto <vvalotto@gmail.com>
2014-03-29 18:55:41 -0300

    Ajustes Presentación Modulo2

---
+++ Demostraciones/Modulo 2/14 - Eventos y Delegados/14a - Eventos y Delegados/Empleado.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_2
+{
+    class Empleados
+    {
+        //public delegate void DatosCambiadosEventHandler();
+        //public event DatosCambiadosEventHandler DatosCambiados;
+
+        public event EventHandler DatosCambiados;
+
+        private string _nombre = string.Empty;
+
+        public string Nombre
+        {
+            get {return _nombre;}
+
+            set
+            {
+               _nombre = value;
+               DatosCambiados(this, new EventArgs());
+                //if (DatosCambiados != null)
+                //{
+                //    DatosCambiados();
+                //}
+            }
+        }
+
+        private string _apellidos = string.Empty;
+        public string Apellidos
+        {
+            get
+            { return _apellidos;}
+            set
+            { _apellidos = value;}
+        }
+
+        private int _edad = 0;
+        public int Edad
+        {
+            get { return _edad;}
+            set { _edad = value;}
+        }
+
+        public override string ToString()
+        {
+            return _apellidos + "," + _nombre;
+        }
+
+        public Empleados(string nombre, string apellidos, int edad)
+        {
+            this._nombre = nombre;
+            this._apellidos = apellidos;
+            this._edad = edad;
+        }
+
+    }
+}
